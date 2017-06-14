ba43a7ebf4a0c7303333ba70ceb6385f1a1d741a
lgc850106 <lgc850106@qq.com>
2017-06-14 22:31:42 +0800

    0614

---
+++ src/com/marakana/android/yamba/StatusActivity.java
@@ -0,0 +1,57 @@
+package com.marakana.android.yamba;
+
+//import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;	//
+import android.view.Menu;
+import android.view.View;	//
+import android.view.View.OnClickListener;	//
+import android.widget.Button;	//
+import android.widget.EditText;	//
+//import android.view.MenuItem;
+
+
+public class StatusActivity extends Activity implements
+										OnClickListener{
+
+	private static final String TAG = "StatusActivity";
+	private EditText editStatus;	//
+	private Button buttonTweet;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status);
+        
+        editStatus = (EditText) findViewById(R.id.editStatus);
+        buttonTweet = (Button) findViewById(R.id.buttonTweet);
+        
+        buttonTweet.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+    	String status = editStatus.getText().toString();
+    	Log.d(TAG, "onClicked with status:" + status);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.status, menu);
+        return true;
+    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+}
